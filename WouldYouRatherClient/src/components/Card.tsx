7093c838c174b581cac5d38c84a0b5d9a5f742f7
karlsb <karlspetsblomberg@gmail.com>
2024-12-06 09:16:43 +0100

    color changes on choice

--- WouldYouRatherClient/src/components/Card.tsx
+++ WouldYouRatherClient/src/components/Card.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import className from 'classnames'
 
 type CardProps = {
   text : string
@@ -10,8 +11,14 @@ type CardProps = {
 
 export function Card(props: CardProps){
   const [text, setText] = useState("")
-  const [additional, setAdditional] = useState("")
-  
+  const [additional, setAdditional] = 
+     useState( props.side === "left" ? 
+        "flex justify-center items-center h-1/2 rounded-full mr-2 m-0 auto p-6 bg-secondary transition-colors duration-400 mr-2"
+       :"flex justify-center items-center h-1/2 rounded-full mr-2 m-0 auto p-6 bg-secondary transition-colors duration-400 ml-2"
+
+    )
+  const [picked, setPicked] = useState(false)
+
   let classes = "flex justify-center items-center h-1/2 rounded-full mr-2 m-0 auto p-6 bg-secondary transition-colors duration-400"
   classes = props.side === "left" ?
         classes + " mr-2"
@@ -21,26 +28,40 @@ export function Card(props: CardProps){
 
   const onPress = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     e.preventDefault()
+    setPicked(true)
     if(!props.choiceMade){
-        setAdditional("dummy-class bg-neutral text-primary")
         props.handleClick(e)
     }
   }
 
   useEffect(() => {
+    if(picked){
+    setAdditional((prev) => {
+      const updatedClasses = prev
+        .split(" ")
+        .filter((cls) => cls !== "bg-secondary") // Remove the target class
+        .join(" ");
+      return `${updatedClasses}  bg-neutral text-primary`; // Add the new class
+    })
+    }
+  },[picked])
+
+  useEffect(() => {
     if(!props.choiceMade){
-        setAdditional("")
+        setAdditional("flex justify-center items-center h-1/2 rounded-full mr-2 m-0 auto p-6 bg-secondary transition-colors duration-400 mr-2")
     }
   },[props.choiceMade])
 
   useEffect(() => {
     setText(props.text)
+    setPicked(false)
   },[props])
- 
+
+
   return (
     <>
-      <div onClick = {onPress} className={classes + additional}>
-        <h2 key={text} className="text-center text-balance font-mono font-bold text-2xl animate-fade text-light" >{text}</h2>
+      <div onClick = {onPress} className={additional}>
+        <h2 key={text} className="text-center text-balance font-mono font-bold text-2xl animate-fade text-light">{text}</h2>
       </div>
     </>
   )  
